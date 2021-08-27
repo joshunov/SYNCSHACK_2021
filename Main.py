b46951c94f3bf33c7631459c07f5a7ac24a62de0
joshunov <joshnov7@gmail.com>
2021-08-27 21:39:59 +1000

    Create Main.py
    Started working on the classes

---
+++ Main.py
@@ -0,0 +1,34 @@
+from PIL import Image                                                                                
+img = Image.open('/Users/joshuanovick/Desktop/Screenshot.png')
+img.show() 
+
+#each game chooses how drinks are dished out, every game has an output 
+
+
+class Game:
+    def __init__(self, players:Player):
+        self.leaderboard = []
+        self.player_list = []
+
+        #include more game info...
+    def leaderboard_print(self):
+        void
+    
+    def create_player_list(self):
+        void
+
+class Player:
+    def __init__(self,Game:Game):
+        self.name = " "
+        self.weight = 0
+        self.bac = 0
+        self.gender = " " # M or F SAY BIOLOGICAL SEXXX
+        self.point = 0
+    
+    def drink(self)
+
+adam.drink()
+
+#EVERY GAME FO MUST HAVE STANDARD OUTPUT/INPUT
+#Input will be 
+
