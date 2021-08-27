c10d468ed7c24b42bdb519a41c46a3b0f3d1a57a
joshunov <joshnov7@gmail.com>
2021-08-28 08:08:35 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -11,10 +11,7 @@ class Player:
         self.bac = 0
         self.gender = " " # M or F SAY BIOLOGICAL SEXXX
         self.point = 0
-    
-    def enter_name(self):
-        #FINISH
-        return
+        self.is_host = bool
     
     def drink(self):
         #FINISH
@@ -31,16 +28,31 @@ class Game:
         self.player_num = 0
 
         #include more game info...
-    
+    def add_person(self,name):
+        self.player_num += 1
+        name = Player()
+        name.name = name
+
     def create_player_list(self):
         #FINISH
         return
 
-game1 = Game()
-game1.player_num = int(input("How many players will you have"))
-print(game1.player_num)
 
 #EVERY GAME FO MUST HAVE STANDARD OUTPUT/INPUT
 #Input will be
 
 #n = number of players insert later
+
+
+
+#Below is a draft of how the game_begin function will be structured:
+game1 = Game()
+
+print("some long involved but funny rulebook/welcome message")
+n = int(input("please enter the number of players"))
+Gam
+
+for player in range(n):
+    curr_name = input(f"Enter the name of player {player}")
+    
+
