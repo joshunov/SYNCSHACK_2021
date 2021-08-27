823da59c8f7658b781e3ba6a4152f8361ec71c66
joshunov <joshnov7@gmail.com>
2021-08-27 22:15:13 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -4,35 +4,43 @@ img.show()
 
 #each game chooses how drinks are dished out, every game has an output 
 
-
-class Game:
-    def __init__(self, players:Player):
-        self.leaderboard = []
-        self.player_list = []
-
-        #include more game info...
-    def leaderboard_print(self):
-        #FINISH
-        return
-    
-    def create_player_list(self):
-        #FINISH
-        return
-
 class Player:
-    def __init__(self,Game:Game):
+    def __init__(self):
         self.name = " "
         self.weight = 0
         self.bac = 0
         self.gender = " " # M or F SAY BIOLOGICAL SEXXX
         self.point = 0
     
+    def enter_name(self):
+        #FINISH
+        return
+    
     def drink(self):
         #FINISH
         return
     
-    def introduce(self,)
+    def introduce(self,):
+         #FINISH
+        return
+    
+class Game:
+    def __init__(self, players: Player ):
+        self.leaderboard = []
+        self.player_list = []
+        self.player_num = 0
 
+        #include more game info...
+    
+    def create_player_list(self):
+        #FINISH
+        return
+
+game1 = Game()
+game1.player_num = int(input("How many players will you have"))
+print(game1.player_num)
 
 #EVERY GAME FO MUST HAVE STANDARD OUTPUT/INPUT
 #Input will be
+
+#n = number of players insert later
