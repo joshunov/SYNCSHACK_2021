904ab2f4764239ca4e4f47e3cf9764451c7441aa
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2021-08-28 18:22:40 +1000

    letter by latter baby

--- Horse_racing/horse_racing_2.py
+++ Horse_racing/horse_racing_2.py
@@ -1,5 +1,12 @@
 import pydealer as cd
 from termcolor import colored
+import time
+
+def letter_by_letter(string):
+    for i in string:
+        print(i, end= (''), flush= True)
+        time.sleep(0.035)
+    return ''
 
 
 def horse_racing(player):
@@ -13,18 +20,17 @@ def horse_racing(player):
 
     player_num = len(player)
 
-
-    input("dun da da da DAAA!!\n\nWelcome to the Horse Races!\n\nThe rules of this game are very simple: (press ENTER)")
+    input(letter_by_letter("dun da da da DAAA!!\n\nWelcome to the Horse Races!\n\nThe rules of this game are very simple: (press ENTER)"))
     print("\n\n---------Game Play-----------")
-    print("\n\n1. You should each place a bet on a suit in a deck of cards: Hearts, Diamonds, Spades, Clubs")
-    print("\n2. The bets will be made in drinks, for example, I could bet 3 drinks (sips) on Hearts")
-    print("\n3. Cards will be dealt from the deck and the suit of the card will determine which horse advances")
-    print("\n4. The track is 8 points long so the horse which first reaches 9 points wins!! (press ENTER)")
-    input("\n5. Each time all the horses have advanced to at least one more point, \na card will be dealt which moves the horse of that suit BACK")
-    print("\n\n--------When to Drink--------")
-    print("\n\n1. If your horse didn't win, you must drink the amount of drinks you \nplaced as a bet (that would mean I drink 3 sips)")
-    input("\n2. Those who bet on the winning horse get to hand out double the ammount \nthey bet on (I could hand out 6 drinks)")
-    input("Happy punting, drink responsibly (press ENTER)\n\n\n")
+    letter_by_letter("\n\n1. You should each place a bet on a suit in a deck of cards: Hearts, Diamonds, Spades, Clubs")
+    letter_by_letter("\n2. The bets will be made in drinks, for example, I could bet 3 drinks (sips) on Hearts")
+    letter_by_letter("\n3. Cards will be dealt from the deck and the suit of the card will determine which horse advances")
+    letter_by_letter("\n4. The track is 8 points long so the horse which first reaches 9 points wins!!")
+    input(letter_by_letter("\n5. Each time all the horses have advanced to at least one more point, \na card will be dealt which moves the horse of that suit BACK (press ENTER)"))
+    letter_by_letter("\n\n--------When to Drink--------")
+    letter_by_letter("\n\n1. If your horse didn't win, you must drink the amount of drinks you \nplaced as a bet (that would mean I drink 3 sips)")
+    input(letter_by_letter("\n2. Those who bet on the winning horse get to hand out double the ammount \nthey bet on (I could hand out 6 drinks) (press ENTER)"))
+    input(letter_by_letter("\n\nHappy punting, drink responsibly (press ENTER)\n\n\n"))
 
 
 
@@ -32,11 +38,11 @@ def horse_racing(player):
         bold_name = colored(player[i].name, attrs = ['bold'])
 
         try:
-            player[i].bet = int(input(f"{bold_name}, how many drinks would you like to bet? "))
+            player[i].bet = int(input(letter_by_letter(f"{bold_name}, how many drinks would you like to bet? ")))
         except:
             bad_bet = True
             while bad_bet == True:
-                new_bet = input(f"{bold_name}, you must enter a whole number: ")
+                new_bet = input(letter_by_letter(f"{bold_name}, you must enter a whole number: "))
                 print("\n")
                 try:
                     new_bet = int(new_bet)
@@ -46,13 +52,13 @@ def horse_racing(player):
                     continue
         print("\n\n")
 
-        player[i].suit = input("On which suit (Hearts, Diamonds, Clubs, Spades)? ").lower().capitalize()
+        player[i].suit = input(letter_by_letter("On which suit (Hearts, Diamonds, Clubs, Spades)? ")).lower().capitalize()
         print("\n\n")
         player_suit = True
         if player[i].suit not in SUITS:
             player_suit = False
             while player_suit != True:
-                new_suit = input(f"{bold_name}, you must enter one of the following: Clubs, Diamonds, Hearts, Spades: ")
+                new_suit = input(letter_by_letter(f"{bold_name}, you must enter one of the following: Clubs, Diamonds, Hearts, Spades: "))
                 new_suit = new_suit.lower().capitalize().strip()
                 player[i].suit = new_suit
                 if new_suit in SUITS:
@@ -92,12 +98,12 @@ def horse_racing(player):
             print("\n")
         
         print("\n\n")
-        input("Press ENTER to turn the next card.")
+        input(letter_by_letter("Press ENTER to turn the next card."))
         print("\n\n")
 
         if racer_dict["Ace of Hearts"] > counter and racer_dict["Ace of Spades"] > counter and racer_dict["Ace of Diamonds"] > counter and racer_dict["Ace of Clubs"] > counter:
             counter += 1
-            input(f"All horses have reached {counter} points. Which horse will fall one point behind? (press ENTER)\n")
+            input(letter_by_letter(f"All horses have reached {counter} points. Which horse will fall one point behind? (press ENTER)\n"))
             reverse = deck.deal(1)
             print(colored(f"{reverse}\n", 'red', attrs= ['bold']))
         
@@ -122,7 +128,7 @@ def horse_racing(player):
                 print("\n")
 
             print("\n\n\n")
-            input("Press ENTER to turn the next card.")
+            input(letter_by_letter("Press ENTER to turn the next card."))
             print("\n\n")
 
 
@@ -136,20 +142,20 @@ def horse_racing(player):
         if player[i].suit in winning_card:
             winning_players.append(player[i].name)
 
-    print("Congratumalations to the winners:")
+    letter_by_letter("Congratumalations to the winners:")
     for name in winning_players:
-        print(colored(f"{name}\n", 'yellow', attrs= ['bold']))
+        letter_by_letter(colored(f"{name}\n", 'yellow', attrs= ['bold']))
 
-    print("Now for all you losers who don't know how to punt, lets see how much you have to drink:\n")
+    letter_by_letter("Now for all you losers who don't know how to punt, lets see how much you have to drink:\n")
     for i in range(player_num):
         if player[i].name in winning_players:
             bold_name = colored(f"{player[i].name}", 'yellow', attrs= ['bold'])
             bold_sips = colored(f"{player[i].bet}", attrs= ['bold'])
-            print(f"{bold_name}, you get to dish out {player[i].bet * 2} sips! Good job\n")
+            letter_by_letter(f"{bold_name}, you get to dish out {player[i].bet * 2} sips! Good job\n")
         else:
             bold_name = colored(f"{player[i].name}", 'red', attrs= ['bold'])
             bold_sips = colored(f"{player[i].bet}", attrs= ['bold'])
-            print(f"{bold_name}, you gotta pick up your game. Drink {bold_sips} sips\n")
+            letter_by_letter(f"{bold_name}, you gotta pick up your game. Drink {bold_sips} sips\n")
             for j in range(player[i].bet):
                 player[i].drink()
         
