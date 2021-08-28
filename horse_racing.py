2fa087032d4377f033e0e6ca03fc9fa3c1099d45
AdamRiesel <89631388+AdamRiesel@users.noreply.github.com>
2021-08-28 10:56:35 +1000

    Started
    trying to figutre out this class bs

--- horse_racing.py
+++ horse_racing.py
@@ -1,7 +1,43 @@
-from deck_of_cards import deck_of_cards as dc
+import pydealer as cd
 
-deck_obj = dc.DeckOfCards
+class Player:
+    def __init__(self):
+        self.name = "John"
+        self.drinks = 0
+        self.suit = "Diamonds"
 
-for i in range(52):
-    card = deck_obj.give_random_card
-    print(card)
+    def bet(self):
+        
+        
+
+deck = cd.Deck(rebuild = True, re_shuffle = True)
+deck.shuffle()
+
+RACERS = ["Ace of Hearts", "Ace of Diamonds", "Ace of Spades", "Ace of Clubs"]
+
+deck.get_list(RACERS)
+
+print("dun da da da DAAA!!\nWelcome to the Horse Races!\nThe rules of this game are very simple:")
+print("You should each place a bet on a suit in a deck of cards: Hearts, Diamonds, Spades, Clubs")
+print("The bets will be made in drinks, for example, I could bet 3 drinks (sips)")
+print("Cards will be dealt from the deck and the suit of the card will determine which horse advances")
+print("The track is 8 points long so the horse which first reaches 9 points wins!!")
+print("If your horse didn't win, you must drink the amount of drinks you placed as a bet (that would mean I drink 3 sips)")
+print("Those who bet on the winning horse get to hand out double the ammount they bet on (I could hand out 6 drinks)")
+print("Each time all the horses have advanced to a at least one more point, a card will be dealt which moves the horse of that suit BACK")
+print("Happy punting, drink responsibly")
+
+num_play = int(input("How many players? "))
+
+player = Bet.name.input("player name")
+print(mark.name)
+
+
+# players = {}
+# for i in range(num_play):
+#     bet = input("player name: ")
+#     players[name] = 0
+
+# for key in players.keys():
+#     bet = int(input(f"{key}, how many drinks would you like to bet? "))
+#     players[key] = bet
